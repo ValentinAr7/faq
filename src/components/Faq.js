325342e9a8819771a91c528a8ee8d7f04bfb4f39
Valentin <valio990210@gmail.com>
2023-04-05 16:32:00 +0300

    .

---
+++ src/components/Faq.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+
+export function Faq(props) {
+    
+    const [fold, setFold] = useState(false)
+
+    return(
+        <div className="faq">
+            <h2 className="faq-title">
+            {props.title}
+
+            <button className="button" onClick={() => setFold(!fold)}>
+                {fold ? '-' : '+'}
+            </button>
+
+            {fold && <div className="content"></div>}
+            </h2>
+
+        </div>
+    )
+}
