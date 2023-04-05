7222dc742707c928de9c1792f8c08ad6f4710f9c
Valentin <valio990210@gmail.com>
2023-04-05 16:32:05 +0300

    dsa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Faq } from './components/Faq';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pp">
+      <div className='section'>
+        <h1 className='h1'>Freaquently Asked Questions</h1>
+        
+        <Faq
+          title={'How to become software developer?'}
+          desc={'Yes, you can always become software developer if you wish to.'}
+           />
+
+        <Faq
+          title={'Can I start job in 3 months?'}
+          descr={'If you are motivated enough you can laways become developer'}
+         />
+      </div>
+
+
     </div>
   );
 }
